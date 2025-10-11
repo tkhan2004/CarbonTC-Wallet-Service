22afb19f3436506519b8cb5cde29c55dd7f192ee
khangnguyen <tcag24042004@gmail.com>
2025-10-11 15:00:29 +0700

    feat: Common response model

---
+++ src/main/java/com/carbontc/walletservice/payload/ApiResponse.java
@@ -0,0 +1,19 @@
+package com.carbontc.walletservice.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.Instant;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class ApiResponse<T> {
+
+    private Boolean success;
+    private String message;
+    private T data;
+    private List<String>  errors;
+    private Instant timestamp =  Instant.now();
+
+}
