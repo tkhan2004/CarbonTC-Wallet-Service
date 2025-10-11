77bcd77a109e3d311d4b68c7a9150168a2c8688d
khangnguyen <tcag24042004@gmail.com>
2025-10-11 14:05:08 +0700

    feat: initialize clean Wallet Service module

---
+++ src/test/java/com/carbontc/walletservice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carbontc.walletservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
