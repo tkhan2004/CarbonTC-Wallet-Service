5ca6f7213165a369dd0b4c3ce72060c126ddd49a
khangnguyen <tcag24042004@gmail.com>
2025-10-12 21:25:34 +0700

    feat: SecurityConfig dev test

---
+++ src/main/java/com/carbontc/walletservice/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.carbontc.walletservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // Tắt CSRF để test dễ hơn
+                .authorizeHttpRequests(auth -> auth
+                        // Cho phép public các endpoint này
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/api-docs/**",
+                                "/api/wallet/**"
+                        ).permitAll()
+                        // Các API khác vẫn cần xác thực
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
