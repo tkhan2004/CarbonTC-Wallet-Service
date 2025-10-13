698b772cd6dd103c23be1104600703ddb141a7bc
khangnguyen <tcag24042004@gmail.com>
2025-10-13 16:21:03 +0700

    refactor: change variable

--- src/main/java/com/carbontc/walletservice/service/EWalletService.java
+++ src/main/java/com/carbontc/walletservice/service/EWalletService.java
@@ -4,13 +4,14 @@ import com.carbontc.walletservice.dto.request.EWalletRequest;
 import com.carbontc.walletservice.dto.response.EWalletResponse;
 import com.carbontc.walletservice.exception.BusinessException;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface EWalletService {
 
     EWalletResponse createWallet(EWalletRequest request) throws BusinessException;
-    EWalletResponse deposit(Long walletId, EWalletRequest request);
-    EWalletResponse withdraw(Long walletId, EWalletRequest request);
-    EWalletResponse getWalletById(Long walletId);
-    List<EWalletResponse> getAllWallets();
+    EWalletResponse deposit(Long walletId, BigDecimal amount) throws  BusinessException;
+    EWalletResponse withdraw(Long walletId, BigDecimal amount ) throws BusinessException;
+    EWalletResponse getMyWalletById(Long userId) throws BusinessException;
+
 }
