7fba5bd6bac9727880b401e05014f723cb000933
khangnguyen <tcag24042004@gmail.com>
2025-10-18 00:53:43 +0700

    feat: vnpay helper hashing

---
+++ src/main/java/com/carbontc/walletservice/util/VNPayUtil.java
@@ -0,0 +1,60 @@
+package com.carbontc.walletservice.util;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+public class VNPayUtil {
+
+    public static String hmacSHA512(final String key, final String data) {
+        try {
+            if (key == null || data == null) { throw new NullPointerException(); }
+            final Mac hmac512 = Mac.getInstance("HmacSHA512");
+            byte[] hmacKeyBytes = key.getBytes(StandardCharsets.UTF_8);
+            final SecretKeySpec secretKey = new SecretKeySpec(hmacKeyBytes, "HmacSHA512");
+            hmac512.init(secretKey);
+            byte[] dataBytes = data.getBytes(StandardCharsets.UTF_8);
+            byte[] result = hmac512.doFinal(dataBytes);
+            StringBuilder sb = new StringBuilder(2 * result.length);
+            for (byte b : result) {
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+        } catch (Exception e) {
+            return "";
+        }
+    }
+
+    public static boolean validateSignature(Map<String, String> params, String secretKey) {
+        String vnp_SecureHash = params.get("vnp_SecureHash");
+        params.remove("vnp_SecureHash");
+        params.remove("vnp_SecureHashType");
+
+        List<String> fieldNames = new ArrayList<>(params.keySet());
+        Collections.sort(fieldNames);
+        StringBuilder hashData = new StringBuilder();
+        Iterator<String> itr = fieldNames.iterator();
+        while (itr.hasNext()) {
+            String fieldName = itr.next();
+            String fieldValue = params.get(fieldName);
+            if ((fieldValue != null) && (fieldValue.length() > 0)) {
+
+                // SỬA LỖI CUỐI CÙNG: Phải ENCODE giá trị khi validate,
+                // giống hệt như lúc tạo chữ ký
+                hashData.append(fieldName);
+                hashData.append('=');
+                try {
+                    hashData.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                } catch (Exception e) { e.printStackTrace(); }
+
+                if (itr.hasNext()) {
+                    hashData.append('&');
+                }
+            }
+        }
+        String calculatedHash = hmacSHA512(secretKey, hashData.toString());
+        return calculatedHash.equals(vnp_SecureHash);
+    }
+}
