26b020b9874b8d3d81eb287a21607eb133fc214c
khangnguyen <tcag24042004@gmail.com>
2025-10-18 00:54:43 +0700

    feat(repository): add jpa query

--- src/main/java/com/carbontc/walletservice/repository/EWalletRepository.java
+++ src/main/java/com/carbontc/walletservice/repository/EWalletRepository.java
@@ -2,7 +2,9 @@ package com.carbontc.walletservice.repository;
 import com.carbontc.walletservice.entity.EWallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface EWalletRepository extends JpaRepository<EWallet, Long> {
+    Optional<EWallet> findByUserId(Long userId);
 }
