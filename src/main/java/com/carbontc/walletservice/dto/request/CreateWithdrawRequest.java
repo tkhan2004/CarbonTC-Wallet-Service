7588a1c64c62593cfd6a1613bafed1dd965b4e97
khangnguyen <tcag24042004@gmail.com>
2025-10-20 14:07:13 +0700

    feat: add withdraw dto

---
+++ src/main/java/com/carbontc/walletservice/dto/request/CreateWithdrawRequest.java
@@ -0,0 +1,25 @@
+package com.carbontc.walletservice.dto.request;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class CreateWithdrawRequest {
+
+    @NotNull(message = "User ID không được để trống")
+    private Long userId; // Tạm thời, sau này sẽ lấy từ JWT token
+
+    @NotNull(message = "Số tiền không được để trống")
+    @DecimalMin(value = "10000.0", message = "Số tiền rút tối thiểu là 10,000")
+    private BigDecimal amount;
+
+    @NotBlank(message = "Số tài khoản ngân hàng không được để trống")
+    private String bankAccountNumber;
+
+    @NotBlank(message = "Tên ngân hàng không được để trống")
+    private String bankName;
+}
---
+++ src/main/java/com/carbontc/walletservice/dto/response/WithdrawRequestResponse.java
@@ -0,0 +1,33 @@
+package com.carbontc.walletservice.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class WithdrawRequestResponse {
+
+
+        private Long requestId;
+
+        private Long userId;
+
+        private BigDecimal amount;
+
+        private String status;
+
+        private String bankAccountNumber;
+
+        private String bankName;
+
+        private LocalDateTime requestedAt;
+
+        private LocalDateTime processedAt;
+}
