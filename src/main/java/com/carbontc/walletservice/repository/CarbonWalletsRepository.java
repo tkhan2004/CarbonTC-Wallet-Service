ee7fa15f1159fa2a01c469cdc12f858abc095ddf
khangnguyen <tcag24042004@gmail.com>
2025-10-21 22:35:59 +0700

    feat: add create carbonwallet, get carbonwallet id, transfer credits

--- src/main/java/com/carbontc/walletservice/repository/CarbonWalletsRepository.java
+++ src/main/java/com/carbontc/walletservice/repository/CarbonWalletsRepository.java
@@ -3,7 +3,12 @@ import com.carbontc.walletservice.entity.CarbonWallets;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CarbonWalletsRepository extends JpaRepository<CarbonWallets, Long>
 {
+    boolean existsByOwnerId(Long ownerId);
+
+    Optional<CarbonWallets> findByOwnerId(Long ownerId);
 }
