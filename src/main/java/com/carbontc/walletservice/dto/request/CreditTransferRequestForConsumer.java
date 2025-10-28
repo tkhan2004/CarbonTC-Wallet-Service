31b7ab79fbed92bb5925d06ef8b40507fa8c5576
khangnguyen <tcag24042004@gmail.com>
2025-10-28 22:33:02 +0700

    feat: new dto for credit transfer

---
+++ src/main/java/com/carbontc/walletservice/dto/request/CreditTransferRequestForConsumer.java
@@ -0,0 +1,23 @@
+package com.carbontc.walletservice.dto.request;
+
+import com.carbontc.walletservice.entity.status.TransferType;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class CreditTransferRequestForConsumer {
+    @NotNull(message = "ID người nhận không được trống")
+    private String toUserId;
+
+    @NotNull(message = "Số lượng không được trống")
+    @DecimalMin(value = "0.1", message = "Số lượng chuyển tối thiểu là 0.1")
+    private BigDecimal amount; // Số lượng tín chỉ
+
+    @NotNull(message = "Loại chuyển khoản không được trống")
+    private TransferType transferType;
+
+    private String referenceId;
+}
