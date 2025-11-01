316508581b46caa4d0876e9c3d13073e9f7f10b4
khangnguyen <tcag24042004@gmail.com>
2025-11-01 22:19:16 +0700

    feat: add FeeStatus and refactor FeeType

---
+++ src/main/java/com/carbontc/walletservice/entity/status/FeeStatus.java
@@ -0,0 +1,6 @@
+package com.carbontc.walletservice.entity.status;
+
+public enum FeeStatus {
+    PENDING_WITHDRAWAL, // Phí đang chờ được rút
+    WITHDRAWN
+}
--- src/main/java/com/carbontc/walletservice/entity/status/FeeType.java
+++ src/main/java/com/carbontc/walletservice/entity/status/FeeType.java
@@ -1,6 +1,5 @@
 package com.carbontc.walletservice.entity.status;
 
 public enum FeeType {
-    PENDING_WITHDRAWAL, // Phí đang chờ được rút
-    WITHDRAWN           // Phí đã được rút (đã chuyển vào ví Admin)
+    PLATFORM_FEE        // Phí đã được rút (đã chuyển vào ví Admin)
 }
